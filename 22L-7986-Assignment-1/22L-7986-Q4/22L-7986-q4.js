703f1bda420e1cda3b03dd18aea80c1ee50c7c81
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-02-14 00:03:14 +0500

    Q4 Done

---
+++ 22L-7986-Assignment-1/22L-7986-Q4/22L-7986-q4.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const addButton = document.getElementById("addButton");
+    if (addButton) {
+        addButton.addEventListener("click", displayDate);
+    } else {
+        console.error("Element with ID 'addButton' not found.");
+    }
+});
+
+function displayDate() {
+    const input= document.getElementById("nameInput");
+    if(input.value!="")
+    {
+        console.log(input.value);
+        const list = document.createElement("li");
+        list.textContent=input.value;
+        const ul = document.getElementById("nameList");
+        ul.appendChild(list);
+        for(let i=0;i<ul.children.length;i++)
+        {
+            console.log(ul.children[i].textContent);
+        }
+
+    }
+    else
+    {
+        alert("Write something in the input field");
+        console.error("Write something in the input field");
+    }
+}
