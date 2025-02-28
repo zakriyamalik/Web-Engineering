55ded68bb2c557c94b6126e3e8fb8f674766fb86
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-02-28 14:31:50 +0500

    Complete Mini Project It takes input from the search box and matches with the data geathered from API and shows the suggestions related to the data in the store

---
+++ Mid1-Prepration/projectAjaxJquery/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#search").keyup(function(){
+        var val=$("#search").val();
+        console.log(val)
+        if(val!=""){
+
+            $("#outputBox").show();
+            $.ajax({
+                url:"https://fakestoreapi.com/products",
+                type:"GET",
+                success:function(data){
+                    var final=[]
+                    console.log(data);
+                    var output="";
+                    output=JSON.stringify(data);
+                    $.each(data,function(index,value){
+                           if(value.title.includes(val))
+                           {
+                            var re = new RegExp('(' + val + ')', 'gi');
+                            final.push("<li>"+ value.title.replace(re,'<strong>$1</strong>')+"</li>");
+                           }
+                        
+                    })
+                    console.log("Final   ");
+                    console.log(final)
+                    $("#outputBox").html("<ul>"+final.join("")+"</ul>").show();
+                }
+
+            })
+        }
+        else{
+            $("#outputBox").hide();
+        }
+    })
+    
+})
