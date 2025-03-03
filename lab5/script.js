872313291250adb803574037c58d5eee09e9b779
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-03-03 22:05:08 +0500

    Starting of on Lab 5 on jquery and Ajax

---
+++ lab5/script.js
@@ -0,0 +1,155 @@
+
+// $(".components").hover(function(){
+//     console.log("In hover ")
+
+//     $(this).css("color", "yellow");
+//     }, function(){
+//     $(this).css("color", "white");
+//   });
+
+
+
+  $("#firstEle").click(function(){
+    if(this.style.color == "yellow")
+    {
+        $(".intro1").css("display","none");
+        $(this).css("color", "white");
+    }
+    else
+    {
+        $(this).css("color", "yellow");
+        $(".intro1").css("display","block");
+        $("#bodydiv").addclass("intro1");
+       
+    }
+
+
+    
+  });
+
+$("#secondEle").click(function(){
+    if(this.style.color == "yellow")
+    {
+        $(".intro2").css("display","none");
+        $(this).css("color", "white");
+    }
+    else
+    {
+        $(this).css("color", "yellow");
+        $(".intro2").css("display","block");
+        $("#bodydiv").addclass("intro2");
+       
+    }
+
+    
+    
+  });
+
+  $("#thirdEle").click(function(){
+    if(this.style.color == "yellow")
+    {
+        $(".intro3").css("display","none");
+        $(this).css("color", "white");
+    }
+    else
+    {
+        $(this).css("color", "yellow");
+        $(".intro3").css("display","block");
+        $("#bodydiv").addclass("intro3");
+       
+    }
+
+    
+    
+  });
+
+  $("#fourthEle").click(function(){
+    if(this.style.color == "yellow")
+    {
+        $(".intro4").css("display","none");
+        $(this).css("color", "white");
+    }
+    else
+    {
+        $(this).css("color", "yellow");
+        $(".intro4").css("display","block");
+        $("#bodydiv").addclass("intro4");
+       
+    }
+
+    
+    
+  });
+
+  $("#fifthEle").click(function(){
+    if(this.style.color == "yellow")
+    {
+        $(".intro5").css("display","none");
+        $(this).css("color", "white");
+    }
+    else
+    {
+        $(this).css("color", "yellow");
+        $(".intro5").css("display","block");
+        $("#bodydiv").addclass("intro5");
+       
+    }
+
+    
+    
+  });
+
+  $("#sixthEle").click(function(){
+    if(this.style.color == "yellow")
+    {
+        $(".intro6").css("display","none");
+        $(this).css("color", "white");
+    }
+    else
+    {
+        $(this).css("color", "yellow");
+        $(".intro6").css("display","block");
+        $("#bodydiv").addclass("intro6");
+       
+    }
+
+    
+    
+  });
+
+  $("#sevenEle").click(function(){
+    if(this.style.color == "yellow")
+    {
+        $(".intro7").css("display","none");
+        $(this).css("color", "white");
+    }
+    else
+    {
+        $(this).css("color", "yellow");
+        $(".intro7").css("display","block");
+        $("#bodydiv").addclass("intro7");
+       
+    }
+
+    
+    
+  });
+
+  $("#eightEle").click(function(){
+    if(this.style.color == "yellow")
+    {
+        $(".intro8").css("display","none");
+        $(this).css("color", "white");
+    }
+    else
+    {
+        $(this).css("color", "yellow");
+        $(".intro8").css("display","block");
+        $("#bodydiv").addclass("intro8");
+       
+    }
+
+    
+    
+  });
+
